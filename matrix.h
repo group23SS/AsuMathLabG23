9140ec812d3f760a1a666930fbaaa36c326cedf7
group23SS <33978602+group23SS@users.noreply.github.com>
2017-11-26 22:52:39 +0200

    Add files via upload

---
+++ matrix.h
@@ -0,0 +1,45 @@
+
+#ifndef NUM_H
+#define NUM_H
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string.h>
+
+using namespace std;
+ extern int flag1 ;
+class matrix
+{
+
+    double** mat;
+    int nrow;
+    int ncolumn;
+	 public:
+    int getrow();
+    int getcolumn();
+    void create_matrix(int rows, int columns);
+    void fill(double **mat, int ro, int co , double var  );
+     void breakit(string input);
+    void dimension ( string input );
+    void print_matrix();
+    void destroy();
+    matrix add(matrix b  );
+    matrix operator+(matrix &b);
+    matrix sub(matrix b  );
+    matrix operator-(matrix &b);
+    void print();
+    matrix getCofactor(int r, int c);
+    double getDeterminant();
+    double cofactorDeterminant(int r, int c);
+    matrix matrixOfCofactors();
+    matrix transpose ();
+    matrix multiplicationofconstant (double k) ;
+    matrix inverse () ;
+    matrix multiplication (matrix b );
+    matrix division (matrix b );
+    matrix Rdivision ();
+};
+
+
+
+#endif
